9afd8819f2b0be4b70ceffac2bf35cd24e6e3e10
tubyliec <antikruk@vivaldi.net>
2024-11-28 23:01:29 +0300

    feat: add counter

---
+++ christmas-shop/js/home.js
@@ -0,0 +1,29 @@
+// Identifiers
+
+const DAYS = document.querySelector('.days');
+const HOURS = document.querySelector('.hours');
+const MINUTES = document.querySelector('.minutes');
+const SECONDS = document.querySelector('.seconds');
+
+const TARGET_DATE = new Date('2025-01-01T00:00:00')
+
+
+
+// Counter
+
+
+function countdown() {
+    const CURRENT_DATE = new Date();
+    const REMAINING_TIME = TARGET_DATE - CURRENT_DATE;
+    const REMAINING_DAYS = Math.floor(REMAINING_TIME / (1000 * 60 * 60 * 24));
+    const REMAINING_HOURS = Math.floor(REMAINING_TIME % (1000 * 60 * 60 * 24) / (1000 * 60 * 60));
+    const REMAINING_MINUTES = Math.floor(REMAINING_TIME % (1000 * 60 * 60) / (1000 * 60));
+    const REMAINING_SECONDS = Math.floor(REMAINING_TIME % (1000 * 60) / 1000);
+
+    DAYS.innerText = REMAINING_DAYS.toString().padStart(2, '0')
+    HOURS.innerText = REMAINING_HOURS.toString().padStart(2, '0')
+    MINUTES.innerText = REMAINING_MINUTES.toString().padStart(2, '0')
+    SECONDS.innerText = REMAINING_SECONDS.toString().padStart(2, '0')
+}
+
+setInterval(countdown, 1000);
