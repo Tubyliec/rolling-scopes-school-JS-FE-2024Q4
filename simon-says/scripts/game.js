f62206c826b2a96fe0bf3aede40eebc13b7390c3
tubyliec <antikruk@vivaldi.net>
2025-01-13 23:38:56 +0300

    feat: add show sequence

--- simon-says/scripts/game.js
+++ simon-says/scripts/game.js
@@ -3,7 +3,13 @@ import { createKeyboard, removeAllChildNodes } from "./create-elements.js";
 import { currentElements, actionButtons, gameState } from "./create-dom.js";
 import { numbersArray, lettersArray, mixedArray } from "./data.js";
 
+// identifiers
+
+let sequenceArray = [];
+let keysArray = [];
+
 // Difficulty
+
 function difficultySwap(array, parentElement) {
   array.forEach((element) => {
     element.name.addEventListener("click", function (e) {
@@ -21,32 +27,10 @@ function difficultySwap(array, parentElement) {
     });
   });
 }
+// Disable buttons
 
-//
-let sequenceArray = [];
-
-function createSequense() {
-  const sequenceLength = gameState.liveCounter * 2;
-  const maxIndex = currentElements.keyboardWrapper.childElementCount;
-  let keysArray = [];
-  for (let i = 0; i < sequenceLength; i++) {
-    const index = Math.floor(Math.random() * maxIndex);
-    if (maxIndex === 10) {
-      keysArray = numbersArray;
-    } else if (maxIndex === 26) {
-      keysArray = lettersArray;
-    } else {
-      keysArray = mixedArray;
-    }
-
-    const char = keysArray[index];
-    sequenceArray.push(char);
-  }
-  console.log(sequenceArray);
-}
-
-function startGame() {
-  actionButtons.forEach((element) => {
+function disableActionButtons(buttons) {
+  buttons.forEach((element) => {
     if (
       element === currentElements.startButton ||
       element === currentElements.nextButton
@@ -56,9 +40,61 @@ function startGame() {
       element.classList.remove("no-display");
     }
   });
+}
+
+// Sequense
+
+function showSequense(keys) {
+  let index = 0;
+
+  function showNext() {
+    const key = keys[index];
+    const transform = [{ background: "#ffb84d" }];
+    const timing = {
+      delay: 300,
+      easing: "ease-out",
+      duration: 1000,
+    };
+    if (index >= keys.length) {
+      return;
+    }
+    const animation = key.animate(transform, timing);
+    animation.onfinish = () => {
+      index += 1;
+      showNext();
+    };
+  }
+  showNext();
+}
+
+function createSequense() {
+  const sequenceLength = gameState.liveCounter * 2;
+  const maxIndex = currentElements.keyboardWrapper.childElementCount;
+  let currentArray = [];
+  for (let i = 0; i < sequenceLength; i++) {
+    const index = Math.floor(Math.random() * maxIndex);
+    if (maxIndex === 10) {
+      currentArray = numbersArray;
+    } else if (maxIndex === 26) {
+      currentArray = lettersArray;
+    } else {
+      currentArray = mixedArray;
+    }
+    sequenceArray.push(currentArray[index]);
+    keysArray.push(currentElements.keyboardWrapper.childNodes[index]);
+  }
+  showSequense(keysArray);
+}
+
+// Game
+
+function startGame() {
+  disableActionButtons(actionButtons);
   gameState.liveCounter += 1;
   currentElements.currentRound.textContent = gameState.liveCounter;
   createSequense();
+  console.log(sequenceArray);
+  console.log(keysArray);
 }
 
 // Export
