9c6a20755975f60ee43ee2106c84697728c0036e
tubyliec <antikruk@vivaldi.net>
2025-02-02 02:01:23 +0300

    fix: fix icons

--- nonograms/scripts/utilits.js
+++ nonograms/scripts/utilits.js
@@ -207,17 +207,37 @@ function processToogle() {
       document.body.style.setProperty("--dark-color", "#f9f6f4");
       document.body.style.setProperty("--panel-color", "#308eaf");
       document.body.style.setProperty("--light-color", "#121212");
+      domElements.audioOnImg.style.setProperty(
+        "background-image",
+        `url("./assets/icons/volume-pr.png")`,
+      );
+      domElements.audioOffImg.style.setProperty(
+        "background-image",
+        `url("./assets/icons/volume-muted-pr.png")`,
+      );
+      domElements.themeOnImg.style.setProperty(
+        "background-image",
+        `url("./assets/icons/sun-icon-pr.png")`,
+      );
+      domElements.themeOffImg.style.setProperty(
+        "background-image",
+        `url("./assets/icons/moon-icon-pr.png")`,
+      );
     } else {
       document.body.style.setProperty(
         "--background-color",
         "linear-gradient(90deg, rgba(249,246,244,1) 0%, rgba(249,244,246,1) 100%)",
       );
-      document.body.style.setProperty("--primary-color", "#308eaf");
-      document.body.style.setProperty("--border-color", "#308eaf");
-      document.body.style.setProperty("--dark-color", "#1b1c1d");
-      document.body.style.setProperty("--active-color", "#3663bb");
-      document.body.style.setProperty("--panel-color", "#f9f6f4");
-      document.body.style.setProperty("--light-color", "#f9f6f4");
+      document.body.style.removeProperty("--primary-color");
+      document.body.style.removeProperty("--border-color");
+      document.body.style.removeProperty("--dark-color");
+      document.body.style.removeProperty("--active-color");
+      document.body.style.removeProperty("--panel-color");
+      document.body.style.removeProperty("--light-color");
+      domElements.audioOnImg.style.removeProperty("background-image");
+      domElements.audioOffImg.style.removeProperty("background-image");
+      domElements.themeOnImg.style.removeProperty("background-image");
+      domElements.themeOffImg.style.removeProperty("background-image");
     }
   }
 }
