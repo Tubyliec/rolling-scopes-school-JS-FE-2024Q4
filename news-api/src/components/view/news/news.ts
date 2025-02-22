25ba8f7eebe9a54658d23a79727acd9a74716a78
tubyliec <antikruk@vivaldi.net>
2025-02-22 23:57:20 +0300

    refactor: add types for news.js

---
+++ news-api/src/components/view/news/news.ts
@@ -0,0 +1,49 @@
+import './news.css';
+import { IArticle } from '../../types';
+
+class News {
+    draw(data: IArticle[]) {
+        const news = data.length >= 10 ? data.filter((_item, idx) => idx < 10) : data;
+
+        const fragment = document.createDocumentFragment();
+        const newsItemTemp = document.querySelector('#newsItemTemp');
+        const newsContainer = document.querySelector('.news');
+
+        news.forEach((item, idx) => {
+            if (!(newsItemTemp instanceof HTMLTemplateElement)) {
+                console.log('No news item!');
+                return;
+            }
+
+            const newsClone = newsItemTemp.content.cloneNode(true);
+
+            if (newsClone instanceof DocumentFragment) {
+                if (idx % 2) newsClone.querySelector('.news__item')!.classList.add('alt');
+
+                newsClone.querySelector<HTMLElement>('.news__meta-photo')!.style.backgroundImage = `url(${
+                    item.urlToImage || 'img/news_placeholder.jpg'
+                })`;
+                newsClone.querySelector('.news__meta-author')!.textContent = item.author || item.source.name;
+                newsClone.querySelector('.news__meta-date')!.textContent = item.publishedAt
+                    .slice(0, 10)
+                    .split('-')
+                    .reverse()
+                    .join('-');
+
+                newsClone.querySelector('.news__description-title')!.textContent = item.title;
+                newsClone.querySelector('.news__description-source')!.textContent = item.source.name;
+                newsClone.querySelector('.news__description-content')!.textContent = item.description;
+                newsClone.querySelector('.news__read-more a')!.setAttribute('href', item.url);
+
+                fragment.append(newsClone);
+            }
+        });
+
+        if (newsContainer) {
+            newsContainer.innerHTML = '';
+            newsContainer.appendChild(fragment);
+        }
+    }
+}
+
+export default News;
