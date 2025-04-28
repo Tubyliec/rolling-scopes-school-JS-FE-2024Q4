746cd63af9249dbcd40747ba05257f33d1cf3d07
tubyliec <antikruk@vivaldi.net>
2025-04-28 20:37:12 +0300

    refactor: add union type for placeholder

--- decision-making-tool/src/models/interfaces/input-options.interface.ts
+++ decision-making-tool/src/models/interfaces/input-options.interface.ts
@@ -1,7 +1,7 @@
 import type { EventInputFunction } from '../types/event-input.type';
 
 export interface InputOptions {
-  placeholder: string;
+  placeholder: 'Title' | 'Weight';
   role: 'primary-input' | 'secondary-input';
   callback?: EventInputFunction;
   type?: 'number' | 'text';
