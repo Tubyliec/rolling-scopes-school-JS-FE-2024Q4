3bb090ff0c82fc2e402dd24145a9464ce1c14d86
tubyliec <antikruk@vivaldi.net>
2026-01-05 02:13:04 +0300

    refactor: add linters

--- nonograms/scripts/main.js
+++ nonograms/scripts/main.js
@@ -1,14 +1,13 @@
 //Import
-import { createDom } from "./create-dom.js";
-import { domElements } from "./create-dom.js";
-import { createList, createGame } from "./create-elements.js";
-import { gameState } from "./utilits.js";
+import { createDom } from './create-dom.js';
+import { domElements } from './create-dom.js';
+import { createList, createGame } from './create-elements.js';
 
 //Game
 createDom();
 createGame();
 createList(domElements.fieldset);
-domElements.easyButton.classList.add("dif-btn--active");
-document.getElementById("cross").checked = true;
+domElements.easyButton.classList.add('dif-btn--active');
+document.getElementById('cross').checked = true;
 
-window.addEventListener("resize", createGame);
+window.addEventListener('resize', createGame);
