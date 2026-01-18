ed183be060a7b41a07fe8a030d9c4093207ca61a
tubyliec <antikruk@vivaldi.net>
2026-01-18 23:06:50 +0300

    refactor: rename assets

--- nonograms/scripts/core/data/sounds.js
+++ nonograms/scripts/core/data/sounds.js
@@ -1,9 +1,9 @@
 export const sounds = {
-  pencil: 'assets/audio/pencil.mp3',
-  cross: 'assets/audio/cross.mp3',
-  wipe: 'assets/audio/wipe.mp3',
-  button: 'assets/audio/button.mp3',
-  switch: 'assets/audio/switch.mp3',
-  click: 'assets/audio/click.mp3',
-  fanfare: 'assets/audio/fanfare.mp3',
+  pencil: '/audio/pencil.mp3',
+  cross: '/audio/cross.mp3',
+  wipe: '/audio/wipe.mp3',
+  button: '/audio/button.mp3',
+  switch: '/audio/switch.mp3',
+  click: '/audio/click.mp3',
+  fanfare: '/audio/fanfare.mp3',
 };
