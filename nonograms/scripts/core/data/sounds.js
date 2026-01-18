f709d9673ae3d9539afe4ae67d7c556827f3517f
tubyliec <antikruk@vivaldi.net>
2026-01-19 02:20:45 +0300

    refactor: rebuild dist

--- nonograms/scripts/core/data/sounds.js
+++ nonograms/scripts/core/data/sounds.js
@@ -1,9 +1,9 @@
 export const sounds = {
-  pencil: '/audio/pencil.mp3',
-  cross: '/audio/cross.mp3',
-  wipe: '/audio/wipe.mp3',
-  button: '/audio/button.mp3',
-  switch: '/audio/switch.mp3',
-  click: '/audio/click.mp3',
-  fanfare: '/audio/fanfare.mp3',
+  pencil: 'audio/pencil.mp3',
+  cross: 'audio/cross.mp3',
+  wipe: 'audio/wipe.mp3',
+  button: 'audio/button.mp3',
+  switch: 'audio/switch.mp3',
+  click: 'audio/click.mp3',
+  fanfare: 'audio/fanfare.mp3',
 };
