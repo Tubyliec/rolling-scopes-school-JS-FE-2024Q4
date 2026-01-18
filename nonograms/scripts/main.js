292b24ad0dfa00684a6785c5baab20ec157b4015
Dzmitry <antikruk@vivaldi.net>
2026-01-18 23:12:22 +0300

    Revert "update nonograms"

--- nonograms/scripts/main.js
+++ nonograms/scripts/main.js
@@ -1,13 +1,14 @@
-import { createBasicElements } from './features/ui/create-basic-elements.js';
-import { domElements } from './core/states/dom-elements.js';
-import '../styles/styles.scss';
-import { createGame } from './features/game/create-game';
-import { createList } from './features/ui/board/create-list';
+//Import
+import { createDom } from "./create-dom.js";
+import { domElements } from "./create-dom.js";
+import { createList, createGame } from "./create-elements.js";
+import { gameState } from "./utilits.js";
 
-createBasicElements();
+//Game
+createDom();
 createGame();
 createList(domElements.fieldset);
-domElements.easyButton.classList.add('dif-btn--active');
-document.getElementById('cross').checked = true;
+domElements.easyButton.classList.add("dif-btn--active");
+document.getElementById("cross").checked = true;
 
-window.addEventListener('resize', createGame);
+window.addEventListener("resize", createGame);
