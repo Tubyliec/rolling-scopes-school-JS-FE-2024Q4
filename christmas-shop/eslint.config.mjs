654e069e2ef10f32d6c062f79fd5682fe515b069
tubyliec <antikruk@vivaldi.net>
2026-01-19 22:51:02 +0300

    refactor: rename folders

--- christmas-shop/eslint.config.mjs
+++ christmas-shop/eslint.config.mjs
@@ -6,7 +6,7 @@ export default [
     ignores: ['**/node_modules/**'],
   },
   {
-    files: ['**/*.js'],
+    files: ['**/*.scripts'],
     languageOptions: {
       ecmaVersion: 'latest',
       sourceType: 'module',
